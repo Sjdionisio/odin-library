045158880fb6c2eae9cb2ed8f1a860fcb08e84c2
Sjdionisio <jacobdionisio96@gmail.com>
2025-06-04 05:11:46 +0800

    Add read button and toggle function

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const myLibrary = [];
 
-function Book(title, author, page, id){
+function Book(title, author, page, read){
     this.title = title;
     this.author = author;
     this.page = page;
-    this.id = id;
+    this.id = crypto.randomUUID();
+    this.read = read;
 }
-
-function addBookToLibrary(title, author, page){
-    let id = crypto.randomUUID();
-    myLibrary.push(new Book(title, author, page, id));  
+Book.prototype.toggleRead = function(){
+    this.read = !this.read;
+}
+function addBookToLibrary(title, author, page, read){
+    myLibrary.push(new Book(title, author, page, read));  
 }
 function displayBooks(){ 
     const container = document.querySelector(".books");
@@ -19,37 +21,52 @@ function displayBooks(){
     myLibrary.forEach((book, index) => {
         const div = document.createElement("div");
         const deleteBtn = document.createElement("button");
+        const readBtn = document.createElement("button");
         div.classList.add("book");
         div.textContent = `Title:${book.title} 
         Author:${book.author} 
         Pages:${book.page} 
-        ID: ${book.id}`;
+        ID: ${book.id}  `;
         deleteBtn.classList.add("delete-btn");
         deleteBtn.id = "deleteBtn";
         deleteBtn.textContent = "Delete";
+
+        readBtn.classList.add("read-btn");
+        readBtn.id = "readBtn";
+        readBtn.textContent = `${book.read ? "Read" : "Not Read"}`;
         div.appendChild(deleteBtn);
+        div.appendChild(readBtn);
         container.appendChild(div);
         deleteBtn.addEventListener("click",()=>{
             myLibrary.splice(index,1);
             displayBooks();
-        })
+        });
+        readBtn.addEventListener("click",()=>{
+            book.toggleRead();
+            displayBooks();
+        });
     });
 }
+
 const showButton = document.getElementById("modalBtn");
 const modal = document.getElementById("addModal");
-const title = modal.querySelector("#title");
-const author = modal.querySelector("#author");
-const pages = modal.querySelector("#pages");
 const confirmBtn = modal.querySelector("#confirmBtn");
+
+
 
 showButton.addEventListener("click",()=>{
     modal.showModal();    
 });
-modal.addEventListener("close",(e)=>{
-    addBookToLibrary(title.value,author.value,pages.value);
-    displayBooks(); 
-})
+
 confirmBtn.addEventListener("click", (e)=>{
     e.preventDefault();
+    const addForm = new FormData(document.querySelector("#addForm"));
+    const title = addForm.get("title");
+    const author = addForm.get("author");
+    const pages = addForm.get("pages");
+    const readStatus = addForm.get("read") === "1" ? true : false;
+    console.log(readStatus);
+    addBookToLibrary(title,author,pages,readStatus);
+    displayBooks();
     modal.close();
 })
